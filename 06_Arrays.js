676301e8f6e309b46d8476ec4db9ec8b00451844
Aryamanshu Mishra <93377648+Aryamanshu@users.noreply.github.com>
2024-08-13 10:03:04 +0530

    Add files via upload

---
+++ 06_Arrays.js
@@ -0,0 +1,81 @@
+const  arr = [1,2,3,4,5];
+//console.log(arr);
+
+
+// console.log(arr[0], arr[4]);
+
+// arr.push(6);
+// console.log(arr);
+
+
+// arr.pop(6);
+// console.log(arr);
+
+// arr.shift(arr[0]);
+// console.log(arr);
+
+// arr.unshift(9);
+// console.log(arr);
+
+
+//map 
+
+// const newArr = arr.map(func);
+
+// function func(arr) {
+//     return arr * 2;
+// }
+
+// console.log(newArr);
+
+
+//filter
+
+// const evenNumber = arr.filter(num => num%2==0)
+
+// console.log(evenNumber);
+
+
+// //reduce
+
+// const sumOfAllNumbers = arr.reduce((acc, currentVal) => {
+//     return acc + currentVal
+// } ,0)
+// console.log(sumOfAllNumbers)
+
+
+// for(let i=0; i<=arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+
+// arr.forEach(i => {
+//     console.log(i)
+// });
+
+
+// create a 2d arrray
+
+// const matrix = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+
+// ];
+
+ //console.log(matrix);
+// console.table(matrix);
+
+
+let marks = [
+    ['Megh', 20,60, 'A'],
+    ['Sam', 10,52,'B'],
+    ['Spidey', 28, 43, 'A']
+]
+
+//console.table(marks) //* for better understanding
+
+console.log(marks[0][0])
+console.log(marks[0][1])
+console.log(marks[0][2])
+console.log(marks[0][3])
